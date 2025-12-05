4f8724220921ce2858b3759eb3f9087507f8fbf6
nicoloctm <elp25308@gmail.com>
2025-12-04 23:23:32 -0700

    Add files via upload

---
+++ semana 2 eficiencia/semana 2 eficiencia/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class P
+{
+    static void Main()
+    {
+        for (int i = 0; i <= 15; i++) Console.WriteLine("f(" + i + ") = " + f(i));
+    }
+    static int f(int n)
+    {
+        if (n <= 1) return n;
+        return f(n - 1) + f(n - 2);
+    }
+}
