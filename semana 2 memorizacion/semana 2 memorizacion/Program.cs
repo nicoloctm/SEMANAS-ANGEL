f8dcdd5cdf7b6aa80f7885b0bcf084d0dec2ab84
nicoloctm <elp25308@gmail.com>
2025-12-04 23:24:24 -0700

    Add files via upload

---
+++ semana 2 memorizacion/semana 2 memorizacion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Fib(40)); // 102334155
+        Console.WriteLine(Fib(50)); // 12586269025
+    }
+
+    static int FibMemo(int n, Dictionary<int, int> memo)
+    {
+        if (n <= 1) return n;
+        if (memo.TryGetValue(n, out int v)) return v;
+        v = FibMemo(n - 1, memo) + FibMemo(n - 2, memo);
+        memo[n] = v;
+        return v;
+    }
+
+    static int Fib(int n) => FibMemo(n, new Dictionary<int, int>());
+}
